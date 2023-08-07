5526f54df9d7d479fcfb37a4abb3b82b071cbac4
EssarhaniYoussef <youssef.essarhani@etu.uae.ac.ma>
2023-08-07 23:25:00 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,11 @@
+// Définition de la fonction de somme
+function sum(a, b) {
+  return a + b;
+}
+
+// Utilisation de la fonction et affichage du résultat dans la console
+const num1 = 5;
+const num2 = 10;
+const result = sum(num1, num2);
+
+console.log(`La somme de ${num1} et ${num2} est : ${result}`);
